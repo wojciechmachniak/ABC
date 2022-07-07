a5a240a0bf7d3add5cc14a09d6e22cc949b95c10
wojciechmachniak <null>
2022-07-07 07:45:49 +0000

    First component

---
+++ components/u-hello-component/compoenentCode.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Hello</h1>
+  </div>
+</template>
+
+<script>
+module.exports = {
+  props: {},
+  data() {
+    return {}
+  },
+  beforeCreate() {},
+  created() {},
+  beforeMount() {},
+  mounted() {},
+  beforeUpdate() {},
+  updated() {},
+  beforeDestroy() {},
+  destroyed() {},
+}
+</script>
+
+<style>
+</style>
